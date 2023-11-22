a560fa3d9b2a074f10f2c55e1e3a5bf808616ab7
Tommy Omotoso <omotoso8@gmail.com>
2023-11-22 02:57:02 +0000

    Update regex.py

--- regex.py
+++ regex.py
@@ -2,17 +2,32 @@ import rstr
 from tdda import rexpy
 
 # create accept strings
-acceptRE = []
-for x in range(10): 
-    acceptRE.append(rstr.xeger('[A-Z0-9]{3} [A-Z0-9]{4}'))
+acceptStrings = []
+for r in range(10): 
+    acceptStrings.append(rstr.xeger('[A-Z0-9]{3} [A-Z0-9]{4}'))
+
+#print(acceptStrings)
 
 #create reject strings
-rejectRE = []
-for x in range(10): 
-    rejectRE.append(rstr.xeger('[A-Z0-9]{3} [A-Z0-9]{4}'))
+rejectStrings = []
+for r in range(10): 
+    rejectStrings.append(rstr.xeger('reject'))
 
+#print(rejectStrings)
 
-results = rexpy.extract(acceptRE)
-print('Number of regular expressions found: %d' % len(results))
-for r in results:
-    print('   ' + r)
+#regular expressions created from input data
+acceptRE = rexpy.Extractor(acceptStrings) #acceptRE.results.rex
+rejectRE = rexpy.Extractor(rejectStrings) #reject.results.rex
+
+#evaluate no. strings that match RE
+for acc1, n1 in zip(acceptRE.results.rex, acceptRE.coverage(dedup=True)):
+    print('%d accept string examples are matched by the acceptRE: %s' % (n1, acc1))
+
+for acc2, n2 in zip(acceptRE.results.rex, rejectRE.coverage(dedup=True)):
+    print('%d reject string examples are matched by the acceptRE: %s' % (n2, acc2))
+
+for re1, n3 in zip(rejectRE.results.rex, rejectRE.coverage(dedup=True)):
+    print('%d reject string examples are matched by the rejectRE: %s' % (n3, re1))
+
+for re2, n4 in zip(rejectRE.results.rex, acceptRE.coverage(dedup=True)):
+    print('%d accept string examples are matched by the rejectRE: %s' % (n4, re2))
